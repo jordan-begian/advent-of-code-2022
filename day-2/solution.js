b92862d2a82c3e8c4dd14dc6374dd289f1fc59d8
Jordan Begian <46720056+jordanbegian@users.noreply.github.com>
2022-12-17 01:48:02 -0500

    add logic to convert puzzle input into array of player rounds

--- day-2/solution.js
+++ day-2/solution.js
@@ -1,1 +1,35 @@
-var fs = require('fs');
+import { readFileSync } from "fs";
+
+class Player {
+  constructor(choice, score) {
+    this.choice = choice;
+    this.choiceValue = (function () {
+      switch (choice) {
+        case "A":
+        case "X":
+          return 1;
+        case "B":
+        case "Y":
+          return 2;
+        case "C":
+        case "Z":
+          return 3;
+      }
+    })();
+    this.score = score;
+  }
+}
+
+const file = "day-2/resources/day-2-puzzle-input.txt";
+const rounds = getRounds(file);
+
+console.log(rounds);
+
+function getRounds(file) {
+  const puzzleInput = [];
+  readFileSync(file)
+    .toString()
+    .split("\n")
+    .forEach((item) => puzzleInput.push(item.split(" ")));
+  return puzzleInput.map((round) => round.map((player) => new Player(player)));
+}
