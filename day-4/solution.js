d22919989eacff90e250a09db25ab03bd2a0eb8a
Jordan Begian <46720056+jordanbegian@users.noreply.github.com>
2022-12-24 16:26:55 -0500

    start function to find fully contained ranges

--- day-4/solution.js
+++ day-4/solution.js
@@ -17,9 +17,10 @@ class Elf {
 
 const filePath = "day-4/resources/day-4-puzzle-input.txt";
 
-const puzzleInput = getPuzzleInput(filePath);
+const elfPairs = getPuzzleInput(filePath);
+const pairs = findFullyContainedRanges(elfPairs);
 
-console.log(puzzleInput);
+console.log(pairs);
 
 // reads file from file path, separates values by new line,
 // and creates an elf based on start and end value
@@ -33,4 +34,11 @@ function getPuzzleInput(filePath) {
         .map((item) => item.split("-"))
         .map((item) => new Elf(Number(item[0]), Number(item[1])))
     );
+}
+
+function findFullyContainedRanges(elfPairs) {
+  return elfPairs.map((pair) =>
+    pair[0].range.filter((range) => pair[1].range.includes(range))
+  );
+  //TODO - find if either pair range is fully contained by overlap values
 }
